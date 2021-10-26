8c64860375d98b2f87bd5cfd4c15aed9f81d40b3
Mysterion186 <kowsikan@hotmail.fr>
2021-10-26 15:46:43 +0200

    maj point d'accès cam carte sabre

--- projet_base.cpp
+++ projet_base.cpp
@@ -15,6 +15,7 @@
  */ 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* 
  * Libraries OpenCV "obligatoires" 
@@ -57,7 +58,7 @@ int main (int argc, char *argv[]) {
 //----------------------------------------------
 // Video acquisition - opening
 //----------------------------------------------
-  VideoCapture cap(0); // le numéro 0 indique le point d'accès à la caméra 0 => /dev/video0
+  VideoCapture cap(4); // le numéro 0 indique le point d'accès à la caméra 0 => /dev/video0
   if(!cap.isOpened()){
     cout << "Error"; return -1;
   
